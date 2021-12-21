422617ac5c93462dbba00a044f300810d7150e50
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2021-12-22 00:59:14 +0300

    Add files via upload

---
+++ TestProject1/ClassesUnderTest/SelfDependent.cs
@@ -0,0 +1,9 @@
+﻿using DependencyInjectionContainer.CustomAttributes;
+
+namespace TestDemo.ClassesUnderTest
+{
+    public class SelfDependent
+    {
+        public SelfDependent(SelfDependent self) {}
+    }
+}
